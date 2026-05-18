9f5e06279eba2ce387e4749ec0362e75fa4c4f86
Denis <denic842rb@gmail.com>
2026-05-18 23:31:20 +0300

    lab2

--- src/fastapi_project/api/v1/router.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-
-from fastapi_project.api.v1.endpoints import health
-
-api_v1_router = APIRouter()
-api_v1_router.include_router(health.router, prefix="/health", tags=["health"])
